506d7f38fbc218912e2de9df5785f64f79ea70a5
Noura Ous <noura.ousfia@gmail.com>
2023-05-24 14:07:54 +0200

    hachage et Salage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, session, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 import os 
 
 app = Flask(__name__)
@@ -29,11 +30,14 @@ def signup():
         username = request.form.get('username')
         password = request.form.get('password')
 
-        new_user = User(username=username, password=password)
+        # Salage et hachage du mot de passe avec un sel aléatoire
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
+
+        new_user = User(username=username, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
 
-        return 'inscritpion reussi !'
+        return 'Inscription réussie !'
     else:
         return render_template('signup.html')
 
@@ -44,15 +48,17 @@ def login():
         username = request.form.get('username')
         password = request.form.get('password')
 
-        user = User.query.filter_by(username=username, password=password).first()
+        user = User.query.filter_by(username=username).first()
 
-        if user:
+        if user and check_password_hash(user.password, password):
             session['username'] = user.username
-            return 'connexion reussi !'
+            return 'Connexion réussie !'
         else:
-            return 'mdp ou username incorrect'
+            return 'Mot de passe ou nom d\'utilisateur incorrect'
     else:
         return render_template('login.html')
+    
+    
     
 @app.route('/logout')
 def logout():
@@ -68,6 +74,8 @@ def admin_page():
         return render_template('admin.html', users=users)
     else:
         return 'NOOOOOOOOOOOO'
+
+
 
 
 
